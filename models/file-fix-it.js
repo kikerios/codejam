1ae2cb5e774cece5a14e8682ac1bbda9f6bfd81c
kikerios <web.lrios@gmail.com>
2017-01-12 22:53:28 -0600

    add deepPopulate plugin

--- models/file-fix-it.js
+++ models/file-fix-it.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose'), Schema = mongoose.Schema;
+var mongoose = require('mongoose'), Schema = mongoose.Schema, deepPopulate = require('../lib/plugin.js')(mongoose)
+        ;
 var fileFixItSchema = new Schema({
     path: {
         type: String,
@@ -11,6 +12,9 @@ var fileFixItSchema = new Schema({
     mkdir: {
         type: Number,
         required: true
-    }
+    },
+    childs: [{type: Schema.Types.ObjectId, ref: 'FileFixIt'}]
+
 });
 module.exports = mongoose.model('FileFixIt', fileFixItSchema); 
+fileFixItSchema.plugin(deepPopulate)
